a289514ffd555775033821c8739ade7ee705d219
Fatemeh Hashemi <fatemeh.thxli03.hashemi@gmail.com>
2024-06-07 17:34:58 +0330

    working on student/professor page

--- lib/student/Studentprofessor.dart
+++ lib/student/Studentprofessor.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 void main() {
-  runApp(const MaterialApp(home: StudentProfessor(),));
+  runApp(const MaterialApp(home: StudentProfessor(),
+  debugShowCheckedModeBanner: false,));
 }
 
   class StudentProfessor extends StatelessWidget {
@@ -9,8 +10,33 @@ void main() {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+       appBar: AppBar(
+          backgroundColor: const Color.fromARGB(245, 239, 176, 216),
+          shadowColor: const Color.fromARGB(100, 51, 22, 50) ,
+       ),
        body: Container(
          decoration: const BoxDecoration(color:  Color.fromARGB(255, 254, 196, 234)),
+         child:  Stack(
+           children:<Widget> [
+             Positioned(
+               top: 75,
+               left: 85,
+               child: Text(
+                 "              are you \n Student or Professor? " ,
+                 style: TextStyle(
+                   color: Color.fromARGB(255, 24, 21, 66) ,
+                   fontSize: 30,
+                   fontWeight: FontWeight.w700,
+                   fontFamily: 'chunck',
+                 ),
+               )
+             ),
+             Positioned(
+                 top: 190,
+                 left: 40,
+                 child: Image.asset('images/students.png'))
+           ],
+         ),
 
        )
 
